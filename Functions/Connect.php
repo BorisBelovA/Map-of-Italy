202df70473cad0877b54c9e7ce2dd80481404ac6
Boris <33251596+BorisBelovA@users.noreply.github.com>
2018-02-09 20:37:41 +0300

    Update Connect.php

--- Functions/Connect.php
+++ Functions/Connect.php
@@ -3,6 +3,6 @@
 	define('DB_USER', 'root');
 	define('DB_PASSWORD','');
 	define('DB','maps');
-	$link = new mysqli('localhost','root','', 'maps');
+	$link = new mysqli(DB_HOST,DB_USER,DB_PASSWORD, DB);
 	$link->query('SET NAMES "utf8"');
 ?>
